6bc3570999b6dceea6c8934eb4fd8c9afa876ef3
anil kadam <anilkadam210@gmail.com>
2020-07-13 21:09:50 +0530

    Added component in module

--- src/app/productreactiveform/app.productreactiveform.component.ts
+++ src/app/productreactiveform/app.productreactiveform.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-productreactiveform-component",
-  template: "./app.productreactiveform.component.html",
+  templateUrl: "./app.productreactiveform.component.html",
 })
-export class ProductReactiveFormComponent implements OnInit {}
+export class ProductReactiveFormComponent implements OnInit {
+  ngOnInit() {}
+}
