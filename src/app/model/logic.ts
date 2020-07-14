7c063974d2979413cad11dd313418de4b8559b06
anil kadam <anilkadam210@gmail.com>
2020-07-14 23:55:34 +0530

    added delete product and key Ispresent functionality

--- src/app/model/logic.ts
+++ src/app/model/logic.ts
@@ -1,22 +1,54 @@
-import {Product} from './app.product.model';
+import { Product } from "./app.product.model";
 
 export class Logic {
-    private products: Array<Product>;
+  private products: Array<Product>;
 
-    constructor(){
-        this.products = new Array<Product>();
-    }
+  constructor() {
+    this.products = new Array<Product>();
+  }
 
-    getProducts(): Array<Product> {
-        this.products.push(new Product(1, 'Prd001', 'Laptop', 'Electronics', 'HP', 'Gaming', 120000));
-        this.products.push(new Product(2, 'Prd002', 'Iron', 'Electrical', 'Bajaj', 'Cotton Friendly', 3000));
-        this.products.push(new Product(3, 'Prd003', 'Biscuts', 'Food', 'Parle', 'Glucose', 10));
-        return this.products;
-    }
+  isPresent(productId: string): boolean {
+    if (
+      this.products.filter((product) => product.ProductId === productId)
+        .length > 0
+    )
+      return true;
+    return false;
+  }
 
-    addProduct(prd: Product) : Array<Product> {
-        this.products.push(prd);
-        return this.products;
+  getProducts(): Array<Product> {
+    this.products.push(
+      new Product(1, "Prd001", "Laptop", "Electronics", "HP", "Gaming", 120000)
+    );
+    this.products.push(
+      new Product(
+        2,
+        "Prd002",
+        "Iron",
+        "Electrical",
+        "Bajaj",
+        "Cotton Friendly",
+        3000
+      )
+    );
+    this.products.push(
+      new Product(3, "Prd003", "Biscuts", "Food", "Parle", "Glucose", 10)
+    );
+    return this.products;
+  }
+
+  addProduct(prd: Product): Array<Product> {
+    this.products.push(prd);
+    return this.products;
+  }
+  deleteProduct(productId: string): Array<Product> {
+    if (this.isPresent(productId)) {
+      this.products = this.products.filter(
+        (item) => item.ProductId != productId
+      );
+      return this.products;
     }
+    console.log("Product not present...");
+    this.products;
+  }
 }
-
