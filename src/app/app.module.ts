9933a5505c9040b023c5cf783a8696942123ab79
anil kadam <anilkadam210@gmail.com>
2020-07-14 23:36:26 +0530

    Registered defendencies and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ProductReactiveFormComponent } from "./productreactiveform/ProductReactiveFormComponent";
+import { ProductReactiveFormComponent } from "./productreactiveform/app.productreactiveform.component";
 
 @NgModule({
   declarations: [AppComponent, ProductReactiveFormComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
